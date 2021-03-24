88fd65ab244c0b7648312816501cc2909ab5bd42
Tobias dpausp <dpausp@posteo.de>
2021-03-24 00:13:50 +0000

    Use niv from nixpkgs

--- shell.nix
+++ shell.nix
@@ -4,7 +4,6 @@ with builtins;
 let
   sources_ = if (sources == null) then import ./nix/sources.nix else sources;
   inherit (sources_) nixpkgs;
-  niv = (import sources_.niv { }).niv;
   vvvoteShellInputs = (import "${sources_.nix-ekklesia-vvvote}/nix/deps.nix" { inherit sources; }).shellTools;
   portalShellInputs = (import "${sources_.ekklesia-portal}/nix/deps.nix" { inherit sources; }).shellTools;
   pkgs = import nixpkgs { config = {}; };
@@ -14,7 +13,7 @@ in
 
 pkgs.mkShell {
 
-  buildInputs = [ pkgs.nixops niv python ] ++ vvvoteShellInputs;
+  buildInputs = [ pkgs.nixops pkgs.niv python ] ++ vvvoteShellInputs;
 
   shellHook = ''
     export NIX_PATH="nixpkgs=${nixpkgs}:."
